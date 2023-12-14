277874a07380c3a5ff069664a02f974929b4debd
lc <11928047+cplk@user.noreply.gitee.com>
2023-12-14 17:00:29 +0800

    first

---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter, Roboto, Poppins } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import { ThemeProvider } from "@/context/ThemeContext";
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
+
+// 引入Inter字体
+const inter = Inter({ subsets: ["latin"] });
+
+// 定义元数据
+export const metadata = {
+  title: "Demo",
+  description: "",
+};
+
+// 定义根组件
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider>
+          <AuthProvider>
+            <div className="container">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </AuthProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
