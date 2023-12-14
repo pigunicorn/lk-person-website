edab53bc82c03a3e81332a1d7c63edcabf851f64
lc <11928047+cplk@user.noreply.gitee.com>
2023-12-14 22:37:11 +0800

    修改文本

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,36 +16,46 @@ const About = () => {
         <div className={styles.imgText}>
           <h1 className={styles.imgTitle}>Digital Storytellers</h1>
           <h2 className={styles.imgDesc}>
-            Handcrafting award winning digital experiences
+          Creating attractive digital products
           </h2>
         </div>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.item}>
-          <h1 className={styles.title}>Who Are We?</h1>
+          <h1 className={styles.title}>Who Am I?</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
-            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
-            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
-            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
-            <br />
-            <br />
-            voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
-            esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
-            officiis voluptatum quo ea eveniet?
+          Hello, I'm LiangKang, a front-end developer passionate about 
+          crafting immersive digital experiences. With expertise in HTML, 
+          CSS, and JavaScript, I bring creativity and precision to every project. 
+          Beyond coding, I enjoy exploring the latest design trends and technologies.
+           Let's connect and bring innovative web solutions to life!
+          </p>
+          <h1 className={styles.title}>Personal Skills</h1>
+          <p className={styles.desc}>      
+          Front end language:JAVASCRIPT, HTML5, CSS3, ES6,TS GIT, 
+          GITEE, REACT, REACT-ROUTER, REACT-HOOKS, REDUX, VUE3,
+           VUE-ROUTER, PINIA, AXIOS, NEXTJS, UNI-APP.
+          <p>
+          Backend language:JAVA, SPRINGBOOT, MYSQL, SQL, NODEJS,
+          MYBATIS.
+          </p>
+          <p>
+          Component library:ELEMENT-PLUS, ANT-DESIGN, MUI, ANT-DESIGN-VUE, VANT.
+          </p>
           </p>
         </div>
         <div className={styles.item}>
-          <h1 className={styles.title}>What We Do?</h1>
+          <h1 className={styles.title}>What I Do?</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
-            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
-            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. -
-            Creative Illustrations
+         <h3>Personal project</h3>
+         1.React management backend:
+         <p>https://gitee.com/cplk/react-demo.git</p>
+         <br />
+         2.Mini Program Mall:
+         <p>https://gitee.com/cplk/uni-app-vue3-ts.git</p>
+         <br />
+         3.Personal homepage:
+         <p>https://gitee.com/cplk/nextjs-demo.git</p>
             <br />
             <br /> - Dynamic Websites
             <br />
