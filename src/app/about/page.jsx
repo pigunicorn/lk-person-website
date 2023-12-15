3689dd0dafd95261c86e3a2462b18fc6ee149284
lc <11928047+cplk@user.noreply.gitee.com>
2023-12-15 12:19:09 +0800

    修改about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,53 +15,47 @@ const About = () => {
         />
         <div className={styles.imgText}>
           <h1 className={styles.imgTitle}>Digital Storytellers</h1>
-          <h2 className={styles.imgDesc}>
-          Creating attractive digital products
-          </h2>
+          <h2 className={styles.imgDesc}>Creating attractive digital products</h2>
         </div>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.item}>
           <h1 className={styles.title}>Who Am I?</h1>
           <p className={styles.desc}>
-          Hello, I'm LiangKang, a front-end developer passionate about 
-          crafting immersive digital experiences. With expertise in HTML, 
-          CSS, and JavaScript, I bring creativity and precision to every project. 
-          Beyond coding, I enjoy exploring the latest design trends and technologies.
-           Let's connect and bring innovative web solutions to life!
+            Hello, I'm LiangKang, a front-end developer passionate about crafting immersive digital experiences. With expertise in HTML, CSS, and JavaScript, I bring creativity and precision to every project. Beyond coding, I enjoy exploring the latest design trends and technologies. Let's connect and bring innovative web solutions to life!
           </p>
           <h1 className={styles.title}>Personal Skills</h1>
           <p className={styles.desc}>      
-          Front end language:JAVASCRIPT, HTML5, CSS3, ES6,TS GIT, 
-          GITEE, REACT, REACT-ROUTER, REACT-HOOKS, REDUX, VUE3,
-           VUE-ROUTER, PINIA, AXIOS, NEXTJS, UNI-APP.
-          <p>
-          Backend language:JAVA, SPRINGBOOT, MYSQL, SQL, NODEJS,
-          MYBATIS.
-          </p>
-          <p>
-          Component library:ELEMENT-PLUS, ANT-DESIGN, MUI, ANT-DESIGN-VUE, VANT.
-          </p>
+            Front end language: JAVASCRIPT, HTML5, CSS3, ES6, TS GIT, GITEE, REACT, REACT-ROUTER, REACT-HOOKS, REDUX, VUE3, VUE-ROUTER, PINIA, AXIOS, NEXTJS, UNI-APP.
+            <br></br>
+            Backend language: JAVA, SPRINGBOOT, MYSQL, SQL, NODEJS, MYBATIS.
+            <br></br>
+       Component library: ELEMENT-PLUS, ANT-DESIGN, MUI, ANT-DESIGN-VUE, VANT.
           </p>
         </div>
         <div className={styles.item}>
           <h1 className={styles.title}>What I Do?</h1>
           <p className={styles.desc}>
-         <h3>Personal project</h3>
-         1.React management backend:
-         <p>https://gitee.com/cplk/react-demo.git</p>
-         <br />
-         2.Mini Program Mall:
-         <p>https://gitee.com/cplk/uni-app-vue3-ts.git</p>
-         <br />
-         3.Personal homepage:
-         <p>https://gitee.com/cplk/nextjs-demo.git</p>
+            Personal project:
             <br />
-            <br /> - Dynamic Websites
+            1. React management backend: <br></br><a href="https://gitee.com/cplk/react-demo.git">https://gitee.com/cplk/react-demo.git</a>
+            <br></br>
             <br />
-            <br /> - Fast and Handy
+
+            2. Mini Program Mall: <br></br><a href="https://gitee.com/cplk/uni-app-vue3-ts.git">https://gitee.com/cplk/uni-app-vue3-ts.git</a>
+            <br></br>
             <br />
-            <br /> - Mobile Apps
+            3. Personal homepage:<br></br> <a href="https://gitee.com/cplk/nextjs-demo.git">https://gitee.com/cplk/nextjs-demo.git</a>
+            
+            <br></br>
+            <br />
+            - Dynamic Websites
+            <br></br>
+            <br />
+            - Fast and Handy
+            <br></br>
+            <br />
+            - Mobile Apps
           </p>
           <Button url="/contact" text="Contact" />
         </div>
