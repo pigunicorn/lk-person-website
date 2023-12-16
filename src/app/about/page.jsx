8fc91beef15d2fe54dd8854cfe54928ae59a8a48
lc <11928047+cplk@user.noreply.gitee.com>
2023-12-16 22:46:11 +0800

    美化

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,22 +15,41 @@ const About = () => {
         />
         <div className={styles.imgText}>
           <h1 className={styles.imgTitle}>Digital Storytellers</h1>
-          <h2 className={styles.imgDesc}>Creating attractive digital products</h2>
+          <h2 className={styles.imgDesc}>
+            Creating attractive digital products
+          </h2>
         </div>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.item}>
           <h1 className={styles.title}>Who Am I?</h1>
           <p className={styles.desc}>
-            Hello, I'm LiangKang, a front-end developer passionate about crafting immersive digital experiences. With expertise in HTML, CSS, and JavaScript, I bring creativity and precision to every project. Beyond coding, I enjoy exploring the latest design trends and technologies. Let's connect and bring innovative web solutions to life!
+            Hello, I'm LiangKang, a front-end developer passionate about
+            crafting immersive digital experiences. With expertise in HTML, CSS,
+            and JavaScript, I bring creativity and precision to every project.
+            Beyond coding, I enjoy exploring the latest design trends and
+            technologies. Let's connect and bring innovative web solutions to
+            life!
           </p>
           <h1 className={styles.title}>Personal Skills</h1>
-          <p className={styles.desc}>      
-            Front end language: JAVASCRIPT, HTML5, CSS3, ES6, TS GIT, GITEE, REACT, REACT-ROUTER, REACT-HOOKS, REDUX, VUE3, VUE-ROUTER, PINIA, AXIOS, NEXTJS, UNI-APP.
+          <p className={styles.desc}>
+            <span className={styles.skillCategory}>
+              <strong>Front end language:</strong>
+            </span>{" "}
+            JAVASCRIPT, HTML5, CSS3, ES6, TS GIT, GITEE, REACT, REACT-ROUTER,
+            REACT-HOOKS, REDUX, VUE3, VUE-ROUTER, PINIA, AXIOS, NEXTJS, UNI-APP.
             <br></br>
-            Backend language: JAVA, SPRINGBOOT, MYSQL, SQL, NODEJS, MYBATIS.
             <br></br>
-       Component library: ELEMENT-PLUS, ANT-DESIGN, MUI, ANT-DESIGN-VUE, VANT.
+            <span className={styles.skillCategory}>
+              <strong>Backend language:</strong>
+            </span>{" "}
+            JAVA, SPRINGBOOT, MYSQL, SQL, NODEJS, MYBATIS.
+            <br></br>
+            <br></br>
+            <span className={styles.skillCategory}>
+              <strong>Component library:</strong>
+            </span>{" "}
+            ELEMENT-PLUS, ANT-DESIGN, MUI, ANT-DESIGN-VUE, VANT.
           </p>
         </div>
         <div className={styles.item}>
@@ -38,24 +57,28 @@ const About = () => {
           <p className={styles.desc}>
             Personal project:
             <br />
-            1. React management backend: <br></br><a href="https://gitee.com/cplk/react-demo.git">https://gitee.com/cplk/react-demo.git</a>
+            1. React management backend: <br></br>
+            <a href="https://gitee.com/cplk/react-demo.git">
+              https://gitee.com/cplk/react-demo.git
+            </a>
             <br></br>
             <br />
-
-            2. Mini Program Mall: <br></br><a href="https://gitee.com/cplk/uni-app-vue3-ts.git">https://gitee.com/cplk/uni-app-vue3-ts.git</a>
+            2. Mini Program Mall: <br></br>
+            <a href="https://gitee.com/cplk/uni-app-vue3-ts.git">
+              https://gitee.com/cplk/uni-app-vue3-ts.git
+            </a>
             <br></br>
             <br />
-            3. Personal homepage:<br></br> <a href="https://gitee.com/cplk/nextjs-demo.git">https://gitee.com/cplk/nextjs-demo.git</a>
-            
+            3. Personal homepage:<br></br>{" "}
+            <a href="https://gitee.com/cplk/nextjs-demo.git">
+              https://gitee.com/cplk/nextjs-demo.git
+            </a>
             <br></br>
-            <br />
-            - Dynamic Websites
+            <br />- Dynamic Websites
             <br></br>
-            <br />
-            - Fast and Handy
+            <br />- Fast and Handy
             <br></br>
-            <br />
-            - Mobile Apps
+            <br />- Mobile Apps
           </p>
           <Button url="/contact" text="Contact" />
         </div>
